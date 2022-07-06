eb6a3277058b9358227d728174ee3ff56c857c17
Elvamei <elva2140@gmail.com>
2022-07-06 22:32:16 +0300

    Create MainPage.java

---
+++ src/main/java/pageObjects/academyShop/MainPage.java
@@ -0,0 +1,48 @@
+//package pageObjects.academyShop;
+//import org.openqa.selenium.WebDriver;
+
+//import javax.swing.*;
+//import java.util.List;
+
+//public class MainPage {
+  //  public WebDriver driver;
+
+    //public MainPage(WebDriver driver) {
+      ///  this.driver = driver;
+
+    //}
+
+    //private By searchInputField = By.id("woocommerce-product-search-field-0");
+    //private By searchResults = By.className("product-type-simple");
+
+    //public WebElement getSearchInputField() {
+      //  return driver.findElement(searchInputField);
+   // }
+
+    //public List<WebElement> getSearchResults(){
+      //  return driver.findElements(searchResults);
+    //}
+
+    //public void searchProduct(String meklejamaFraze){
+      //  getSearchInputField().clear();
+        //getSearchInputField().sendKeys( ... keysToSend: meklejamaFraze);
+        //new Action(driver).sendKeys(Keys.ENTER).perform();
+   // }
+
+
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
