f98f2e4188d436a1293e5b344e7da8ff8afd6263
Elvamei <elva2140@gmail.com>
2022-07-06 22:31:14 +0300

    Create Fox.java

---
+++ src/main/java/classroomFive/inheritance/Fox.java
@@ -0,0 +1,13 @@
+package classroomFive.inheritance;
+
+public class Fox {
+    String name;
+    int legCount;
+
+    public Fox(String name, int legCount) {
+        this.name = name;
+        this.legCount = legCount;
+    }
+
+
+}
